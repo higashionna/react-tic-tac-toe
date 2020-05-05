35ce8d69b8253bb5878d4189eec44c21251c8742
higashionna <higashionna@everlifegroup.com>
2020-05-06 08:34:09 +0900

    Updates

---
+++ precache-manifest.313ba24deb6247e5efd105be3db4ec88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a37d762d37c210ec8118aca6b73e48",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "92e8e67e0f1568af25a6",
+    "url": "/react-tic-tac-toe/static/css/main.df45edbc.chunk.css"
+  },
+  {
+    "revision": "510c3c8541d187121d59",
+    "url": "/react-tic-tac-toe/static/js/2.6d3f0ae2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tic-tac-toe/static/js/2.6d3f0ae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e8e67e0f1568af25a6",
+    "url": "/react-tic-tac-toe/static/js/main.f726b148.chunk.js"
+  },
+  {
+    "revision": "57208936ddd33bb86d2e",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.8a07e9c0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8f4bc7bbd97835dc63d41b458c7de5f.js"
+  "/react-tic-tac-toe/precache-manifest.313ba24deb6247e5efd105be3db4ec88.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tic-tac-toe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
